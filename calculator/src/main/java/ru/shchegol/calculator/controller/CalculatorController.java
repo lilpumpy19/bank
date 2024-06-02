b76c82bf86de37c2d5e9b6c92f189e8643ca7668
lilpumpy19 <100938542+lilpumpy19@users.noreply.github.com>
2024-06-02 18:36:56 +0400

    added swagger configuration

--- calculator/src/main/java/ru/shchegol/calculator/controller/CalculatorController.java
+++ calculator/src/main/java/ru/shchegol/calculator/controller/CalculatorController.java
@@ -1,5 +1,12 @@
 package ru.shchegol.calculator.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -18,6 +25,7 @@ import ru.shchegol.calculator.service.CalculatorOfferService;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Calculator")
 @Slf4j
 @RestController
 @RequestMapping("/calculator")
@@ -27,6 +35,70 @@ public class CalculatorController {
     private final CalculatorCreditService creditService;
 
     @PostMapping("/calc")
+    @Operation(summary = "Calculate credit", description = "Calculates credit based on scoring data and responds with a specific offer or rejection")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful request",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = """
+                                    {
+                                      "amount": 100000,
+                                      "term": 12,
+                                      "firstName": "John",
+                                      "lastName": "Doe",
+                                      "middleName": "Mss",
+                                      "gender": "MALE",
+                                      "birthdate": "1990-01-01",
+                                      "passportSeries": "1234",
+                                      "passportNumber": "123456",
+                                      "passportIssueDate": "2010-01-01",
+                                      "passportIssueBranch": "МВД г. Москва",
+                                      "maritalStatus": "SINGLE",
+                                      "dependentAmount": 0,
+                                      "employment": {
+                                        "employmentStatus": "EMPLOYED",
+                                        "employerINN": "1234567890",
+                                        "salary": 50000,
+                                        "position": "EMPLOYEE",
+                                        "workExperienceTotal": 18,
+                                        "workExperienceCurrent": 3
+                                      },
+                                      "accountNumber": "1234567890",
+                                      "isInsuranceEnabled": true,
+                                      "isSalaryClient": false
+                                    }"""
+                            ))),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = """
+                                    {
+                                      "amount": 100,
+                                      "term": 1,
+                                      "firstName": "n",
+                                      "lastName": "e",
+                                      "middleName": "s",
+                                      "gender": "MALE",
+                                      "birthdate": "2009-01-01",
+                                      "passportSeries": "124",
+                                      "passportNumber": "13456",
+                                      "passportIssueDate": "2030-01-01",
+                                      "passportIssueBranch": "",
+                                      "maritalStatus": "SINGLE",
+                                      "dependentAmount": 0,
+                                      "employment": {
+                                        "employmentStatus": "EMPLOYED",
+                                        "employerINN": "1234567890",
+                                        "salary": 50000,
+                                        "position": "EMPLOYEE",
+                                        "workExperienceTotal": 6,
+                                        "workExperienceCurrent": 1
+                                      },
+                                      "accountNumber": "1234567890",
+                                      "isInsuranceEnabled": true,
+                                      "isSalaryClient": false
+                                    }
+                                    """))),
+
+    })
     public ResponseEntity<CreditDto> calc(@Valid @RequestBody ScoringDataDto scoringData) {
         log.info("Request: {}", scoringData);
         ResponseEntity<CreditDto> response = creditService.calcCredit(scoringData);
@@ -35,6 +107,40 @@ public class CalculatorController {
     }
 
     @PostMapping("/offers")
+    @Operation(summary = "Calculate loan offers", description = "Calculates loan offers based on loan statement data and issues 4 offers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful request",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = """
+                                    {
+                                        "amount": 100000,
+                                        "term": 24,
+                                        "firstName": "John",
+                                        "lastName": "Doe",
+                                        "middleName": "Mss",
+                                        "email": "john.doe@example.com",
+                                        "birthdate": "1985-01-15",
+                                        "passportSeries": "1234",
+                                        "passportNumber": "567890"
+                                    }"""
+                            ))),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(value = """
+                                    {
+                                        "amount": 1000,
+                                        "term": 4,
+                                        "firstName": "J",
+                                        "lastName": "e",
+                                        "middleName": "s",
+                                        "email": "john.doeexample.com",
+                                        "birthdate": "2009-01-15",
+                                        "passportSeries": "123",
+                                        "passportNumber": "56890"
+                                    }"""
+                            ))),
+
+    })
     public ResponseEntity<List<LoanOfferDto>> offers(@Valid @RequestBody LoanStatementRequestDto loanStatement) {
         log.info("Request: {}", loanStatement);
         ResponseEntity<List<LoanOfferDto>> response = calculatorService.calcLoanOffers(loanStatement);
--- calculator/src/main/java/ru/shchegol/calculator/dto/EmploymentDto.java
+++ calculator/src/main/java/ru/shchegol/calculator/dto/EmploymentDto.java
@@ -1,5 +1,6 @@
 package ru.shchegol.calculator.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import ru.shchegol.calculator.dto.enums.EmploymentStatus;
 import ru.shchegol.calculator.dto.enums.Position;
@@ -13,23 +14,29 @@ import java.math.BigDecimal;
 public class EmploymentDto {
 
     @NotNull
+    @Schema(description = "Employment status of the client", example = "EMPLOYED")
     private EmploymentStatus employmentStatus;
 
     @NotBlank
+    @Schema(description = "Employer's INN", example = "1234567890")
     private String employerINN;
 
     @NotNull
     @DecimalMin(value = "10000")
+    @Schema(description = "Client's monthly salary", example = "50000")
     private BigDecimal salary;
 
     @NotNull
+    @Schema(description = "Client's position", example = "MANAGER")
     private Position position;
 
     @NotNull
     @Min(value = 0)
+    @Schema(description = "Total work experience in months", example = "60")
     private Integer workExperienceTotal;
 
     @NotNull
     @Min(value = 0)
+    @Schema(description = "Current work experience in months at current job", example = "24")
     private Integer workExperienceCurrent;
 }
