75d9b400ad54821f851fe7bef85303f3459a4306
lilpumpy19 <100938542+lilpumpy19@users.noreply.github.com>
2024-06-03 15:33:14 +0400

    changed swagger's answers

--- calculator/src/main/java/ru/shchegol/calculator/controller/CalculatorController.java
+++ calculator/src/main/java/ru/shchegol/calculator/controller/CalculatorController.java
@@ -1,7 +1,6 @@
 package ru.shchegol.calculator.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -9,7 +8,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,66 +35,121 @@ public class CalculatorController {
     @PostMapping("/calc")
     @Operation(summary = "Calculate credit", description = "Calculates credit based on scoring data and responds with a specific offer or rejection")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successful request",
+            @ApiResponse(responseCode = "200",
                     content = @Content(mediaType = "application/json",
                             examples = @ExampleObject(value = """
                                     {
-                                      "amount": 100000,
-                                      "term": 12,
-                                      "firstName": "John",
-                                      "lastName": "Doe",
-                                      "middleName": "Mss",
-                                      "gender": "MALE",
-                                      "birthdate": "1990-01-01",
-                                      "passportSeries": "1234",
-                                      "passportNumber": "123456",
-                                      "passportIssueDate": "2010-01-01",
-                                      "passportIssueBranch": "МВД г. Москва",
-                                      "maritalStatus": "SINGLE",
-                                      "dependentAmount": 0,
-                                      "employment": {
-                                        "employmentStatus": "EMPLOYED",
-                                        "employerINN": "1234567890",
-                                        "salary": 50000,
-                                        "position": "EMPLOYEE",
-                                        "workExperienceTotal": 18,
-                                        "workExperienceCurrent": 3
-                                      },
-                                      "accountNumber": "1234567890",
-                                      "isInsuranceEnabled": true,
-                                      "isSalaryClient": false
-                                    }"""
-                            ))),
-            @ApiResponse(responseCode = "400", description = "Bad request",
-                    content = @Content(mediaType = "application/json",
-                            examples = @ExampleObject(value = """
-                                    {
-                                      "amount": 100,
-                                      "term": 1,
-                                      "firstName": "n",
-                                      "lastName": "e",
-                                      "middleName": "s",
-                                      "gender": "MALE",
-                                      "birthdate": "2009-01-01",
-                                      "passportSeries": "124",
-                                      "passportNumber": "13456",
-                                      "passportIssueDate": "2030-01-01",
-                                      "passportIssueBranch": "",
-                                      "maritalStatus": "SINGLE",
-                                      "dependentAmount": 0,
-                                      "employment": {
-                                        "employmentStatus": "EMPLOYED",
-                                        "employerINN": "1234567890",
-                                        "salary": 50000,
-                                        "position": "EMPLOYEE",
-                                        "workExperienceTotal": 6,
-                                        "workExperienceCurrent": 1
-                                      },
-                                      "accountNumber": "1234567890",
-                                      "isInsuranceEnabled": true,
-                                      "isSalaryClient": false
+                                        "amount": 62175.96,
+                                        "term": 12,
+                                        "monthlyPayment": 5181.33,
+                                        "baseRate": 15,
+                                        "rate": 7.0,
+                                        "psk": 24.3500,
+                                        "isInsuranceEnabled": true,
+                                        "isSalaryClient": true,
+                                        "paymentSchedule": [
+                                            {
+                                                "number": 1,
+                                                "date": "2024-07-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 348.0000,
+                                                "debtPayment": 4833.3300,
+                                                "remainingDebt": 55166.6700
+                                            },
+                                            {
+                                                "number": 2,
+                                                "date": "2024-08-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 319.9667,
+                                                "debtPayment": 4861.3633,
+                                                "remainingDebt": 50305.3067
+                                            },
+                                            {
+                                                "number": 3,
+                                                "date": "2024-09-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 291.7708,
+                                                "debtPayment": 4889.5592,
+                                                "remainingDebt": 45415.7475
+                                            },
+                                            {
+                                                "number": 4,
+                                                "date": "2024-10-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 263.4113,
+                                                "debtPayment": 4917.9187,
+                                                "remainingDebt": 40497.8288
+                                            },
+                                            {
+                                                "number": 5,
+                                                "date": "2024-11-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 234.8874,
+                                                "debtPayment": 4946.4426,
+                                                "remainingDebt": 35551.3862
+                                            },
+                                            {
+                                                "number": 6,
+                                                "date": "2024-12-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 206.1980,
+                                                "debtPayment": 4975.1320,
+                                                "remainingDebt": 30576.2542
+                                            },
+                                            {
+                                                "number": 7,
+                                                "date": "2025-01-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 177.3423,
+                                                "debtPayment": 5003.9877,
+                                                "remainingDebt": 25572.2665
+                                            },
+                                            {
+                                                "number": 8,
+                                                "date": "2025-02-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 148.3191,
+                                                "debtPayment": 5033.0109,
+                                                "remainingDebt": 20539.2556
+                                            },
+                                            {
+                                                "number": 9,
+                                                "date": "2025-03-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 119.1277,
+                                                "debtPayment": 5062.2023,
+                                                "remainingDebt": 15477.0533
+                                            },
+                                            {
+                                                "number": 10,
+                                                "date": "2025-04-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 89.7669,
+                                                "debtPayment": 5091.5631,
+                                                "remainingDebt": 10385.4902
+                                            },
+                                            {
+                                                "number": 11,
+                                                "date": "2025-05-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 60.2358,
+                                                "debtPayment": 5121.0942,
+                                                "remainingDebt": 5264.3960
+                                            },
+                                            {
+                                                "number": 12,
+                                                "date": "2025-06-03",
+                                                "totalPayment": 5181.33,
+                                                "interestPayment": 30.5335,
+                                                "debtPayment": 5150.7965,
+                                                "remainingDebt": 113.5995
+                                            }
+                                        ]
                                     }
-                                    """))),
+                                 """
+                            ))),
+            @ApiResponse(responseCode = "400", description = "Refusal due to negative work experience",
+                    content = @Content(mediaType = "application/json")),
 
     })
     public ResponseEntity<CreditDto> calc(@Valid @RequestBody ScoringDataDto scoringData) {
@@ -109,34 +162,61 @@ public class CalculatorController {
     @PostMapping("/offers")
     @Operation(summary = "Calculate loan offers", description = "Calculates loan offers based on loan statement data and issues 4 offers")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successful request",
+            @ApiResponse(responseCode = "200",
                     content = @Content(mediaType = "application/json",
                             examples = @ExampleObject(value = """
-                                    {
-                                        "amount": 100000,
-                                        "term": 24,
-                                        "firstName": "John",
-                                        "lastName": "Doe",
-                                        "middleName": "Mss",
-                                        "email": "john.doe@example.com",
-                                        "birthdate": "1985-01-15",
-                                        "passportSeries": "1234",
-                                        "passportNumber": "567890"
-                                    }"""
+                                    [
+                                        {
+                                            "statementId": "8f06330c-7350-4e69-a491-7539764a0f51",
+                                            "requestedAmount": 50000,
+                                            "totalAmount": 63727.44,
+                                            "term": 12,
+                                            "monthlyPayment": 5310.62,
+                                            "rate": 11.0,
+                                            "isInsuranceEnabled": true,
+                                            "isSalaryClient": true
+                                        },
+                                        {
+                                            "statementId": "8a8ff3a7-6af5-44bf-a293-0ff166580f0b",
+                                            "requestedAmount": 50000,
+                                            "totalAmount": 63892.92,
+                                            "term": 12,
+                                            "monthlyPayment": 5324.41,
+                                            "rate": 12.0,
+                                            "isInsuranceEnabled": true,
+                                            "isSalaryClient": false
+                                        },
+                                        {
+                                            "statementId": "6f6e3047-bca2-4c85-b821-3f64daa95230",
+                                            "requestedAmount": 50000,
+                                            "totalAmount": 53820.00,
+                                            "term": 12,
+                                            "monthlyPayment": 4485.00,
+                                            "rate": 14.0,
+                                            "isInsuranceEnabled": false,
+                                            "isSalaryClient": true
+                                        },
+                                        {
+                                            "statementId": "a65853d5-77db-4d72-808f-dca5c6e7ad8b",
+                                            "requestedAmount": 50000,
+                                            "totalAmount": 54083.88,
+                                            "term": 12,
+                                            "monthlyPayment": 4506.99,
+                                            "rate": 15,
+                                            "isInsuranceEnabled": false,
+                                            "isSalaryClient": false
+                                        }
+                                    ]
+                                    """
                             ))),
-            @ApiResponse(responseCode = "400", description = "Bad request",
+            @ApiResponse(responseCode = "404",
                     content = @Content(mediaType = "application/json",
-                            examples = @ExampleObject(value = """
+                            examples = @ExampleObject(value ="""
                                     {
-                                        "amount": 1000,
-                                        "term": 4,
-                                        "firstName": "J",
-                                        "lastName": "e",
-                                        "middleName": "s",
-                                        "email": "john.doeexample.com",
-                                        "birthdate": "2009-01-15",
-                                        "passportSeries": "123",
-                                        "passportNumber": "56890"
+                                       "timestamp": "2024-06-03T11:19:53.504+00:00",
+                                           "status": 404,
+                                           "error": "Not Found",
+                                           "path": "/calculator/offers" 
                                     }"""
                             ))),
 
