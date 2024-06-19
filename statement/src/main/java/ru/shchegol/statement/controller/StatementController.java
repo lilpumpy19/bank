a5f957400627e8a2b08e54e57b95b6a1f60ad6ef
lilpumpy19 <100938542+lilpumpy19@users.noreply.github.com>
2024-06-19 17:05:35 +0400

    added swagger in statement service

--- statement/src/main/java/ru/shchegol/statement/controller/StatementController.java
+++ statement/src/main/java/ru/shchegol/statement/controller/StatementController.java
@@ -1,5 +1,11 @@
 package ru.shchegol.statement.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,11 +26,26 @@ public class StatementController {
     private final StatementService statementService;
 
     @PostMapping
+    @Operation(summary = "Calculate loan offers", description = "Receives LoanStatement and sends data to calculate 4 loan offers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    content = @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = LoanOfferDto.class),
+                                    minItems = 4,
+                                    maxItems = 4
+                            )))
+    })
     public ResponseEntity<List<LoanOfferDto>> statement(@Valid @RequestBody LoanStatementRequestDto loanStatement) {
         return ResponseEntity.ok(statementService.getLoanOffers(loanStatement));
     }
 
     @PostMapping("/offer")
+    @Operation(summary = "Select loan offer",
+            description = "The service accepts data to save the selected credit offer to the deal service")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400",description = "Failed to select loan offer")
+    })
     private ResponseEntity<Void> selectOffer(@RequestBody LoanOfferDto loanOffer) {
         statementService.selectOffer(loanOffer);
         return ResponseEntity.ok().build();
